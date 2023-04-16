0be3722c513852188fb0a5b1c8063967818511c0
Asarel Castellanos <asarelc@gmail.com>
2023-04-15 19:06:57 -0700

    updated routing for nested dynamic routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,9 @@
-import Head from "next/head";
-import clientPromise from "../lib/mongodb";
-import { InferGetStaticPropsType } from "next";
-import SchoolCard from "../components/SchoolCard";
-import Link from "next/link";
-
-export default function Home() {
-  // let search = fall_grades.filter((value: { Course: string; }) => {
-  //   return value.Course.match("CS 8")
-  // })
-  // console.log(search);
-
-  // console.log(fall_grades);
-  // console.log(spring_grades);
-
-  let schools: string[] = ['santa-monica-college', 'orange-coast-college'];
+export default  function Home() {
 
   return (
     <div className="container">
       <h1>Welcome!</h1>
       <h4>Click on a school to view grade distributions.</h4>
-      {schools.map((school) => {
-        return (
-          <Link key={school} href={`/${school}`}>
-            <SchoolCard key={school} name={school} />
-          </Link>
-        );
-      })}
     </div>
   );
 }
-
-// export async function getStaticProps() {
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db("santa-monica-college");
-
-//     const fall_grades = await db.collection("fall").find({}).toArray();
-//     const spring_grades = await db.collection("spring").find({}).toArray();
-
-//     return {
-//       props: {
-//         fall_grades: JSON.parse(JSON.stringify(fall_grades)),
-//         spring_grades: JSON.parse(JSON.stringify(spring_grades)),
-//       },
-//     };
-//   } catch (e) {
-//     console.error(e);
-//   }
-// }
---
+++ staticData/supportedSchools.js
@@ -0,0 +1,10 @@
+export const supportedSchools = [
+  {
+    name: "Santa Monica College",
+    database: "santa-monica-college",
+  },
+  {
+    name: "Orange Coast College",
+    database: "orange-coast-college",
+  },
+];
