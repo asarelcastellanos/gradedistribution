f98a00caf2cb3fbdb79898e69aa3465c8649a231
Asarel Castellanos <asarelc@gmail.com>
2023-04-17 17:17:24 -0700

    added NProgress
    - progress bar at top showing load times

--- pages/course/[course].tsx
+++ pages/course/[course].tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import clientPromise from "../../lib/mongodb";
+import NProgress from "nprogress";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -21,58 +22,78 @@ ChartJS.register(
   Legend
 );
 
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 export default function CoursePage({ courses }) {
   const router = useRouter();
   const { course } = router.query;
 
   return (
-    <div>
-      <h1>{course}'s Grade Distribution</h1>
-      {courses.map((individual_course) => {
-        return (
-          <div key={individual_course._id}>
-            <p>Instructor: {individual_course.Instructor}</p>
-            <p>Section: {individual_course.Section}</p>
-            <Bar
-              options={{
-                responsive: false,
-                plugins: {
-                  title: {
-                    display: true,
-                    text: `Total Students = ${individual_course.Grand_Total} `,
+    <>
+      <div>
+        <h1>{course}'s Grade Distribution</h1>
+        {courses.map((individual_course) => {
+          return (
+            <div key={individual_course._id}>
+              <p>Instructor: {individual_course.Instructor}</p>
+              <p>Section: {individual_course.Section}</p>
+              <Bar
+                options={{
+                  responsive: false,
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: `Total Students = ${individual_course.Grand_Total} `,
+                    },
+                    legend: {
+                      display: true,
+                    },
                   },
-                  legend: {
-                    display: true
-                  }
-                },
-              }}
-              data={{
-                labels: ["A", "B", "C", "D", "F", "P", "NP", "IX", "RD", "EW", "W"],
-                datasets: [
-                  {
-                    label: "Students",
-                    data: [
-                      individual_course.A,
-                      individual_course.B,
-                      individual_course.C,
-                      individual_course.D,
-                      individual_course.F,
-                      individual_course.P,
-                      individual_course.NP,
-                      individual_course.IX,
-                      individual_course.RD,
-                      individual_course.EW,
-                      individual_course.W
-                    ],
-                    backgroundColor: "lightblue",
-                  },
-                ],
-              }}
-            />
-          </div>
-        );
-      })}
-    </div>
+                }}
+                data={{
+                  labels: [
+                    "A",
+                    "B",
+                    "C",
+                    "D",
+                    "F",
+                    "P",
+                    "NP",
+                    "IX",
+                    "RD",
+                    "EW",
+                    "W",
+                  ],
+                  datasets: [
+                    {
+                      label: "Students",
+                      data: [
+                        individual_course.A,
+                        individual_course.B,
+                        individual_course.C,
+                        individual_course.D,
+                        individual_course.F,
+                        individual_course.P,
+                        individual_course.NP,
+                        individual_course.IX,
+                        individual_course.RD,
+                        individual_course.EW,
+                        individual_course.W,
+                      ],
+                      backgroundColor: "lightblue",
+                    },
+                  ],
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
