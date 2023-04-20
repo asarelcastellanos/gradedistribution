afc30e0ad0f57b2f0e704fbccce94da3d5f1fae4
Asarel Castellanos <asarelc@gmail.com>
2023-04-19 23:01:21 -0700

    working on design

--- pages/course/[course].tsx
+++ pages/course/[course].tsx
@@ -2,8 +2,6 @@
 import { useRouter } from "next/router";
 import clientPromise from "../../lib/mongodb";
 import { Bar } from "react-chartjs-2";
-import Loading from "./loading";
-import { Suspense } from "react"
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -28,7 +26,7 @@ export default function CoursePage({ courses }) {
   const { course } = router.query;
 
   return (
-    <Suspense fallback={<Loading />}>
+    <>
       <h1>{course}'s Grade Distribution</h1>
       {courses.map((individual_course) => {
         return (
@@ -86,66 +84,7 @@ export default function CoursePage({ courses }) {
           </div>
         );
       })}
-    </Suspense>
-    // <div>
-    //   <h1>{course}'s Grade Distribution</h1>
-    //   {courses.map((individual_course) => {
-    //     return (
-    //       <div key={individual_course._id}>
-    //         <p>Instructor: {individual_course.Instructor}</p>
-    //         <p>Section: {individual_course.Section}</p>
-    //         <Bar
-    //           options={{
-    //             responsive: false,
-    //             plugins: {
-    //               title: {
-    //                 display: true,
-    //                 text: `Total Students = ${individual_course.Grand_Total} `,
-    //               },
-    //               legend: {
-    //                 display: true,
-    //               },
-    //             },
-    //           }}
-    //           data={{
-    //             labels: [
-    //               "A",
-    //               "B",
-    //               "C",
-    //               "D",
-    //               "F",
-    //               "P",
-    //               "NP",
-    //               "IX",
-    //               "RD",
-    //               "EW",
-    //               "W",
-    //             ],
-    //             datasets: [
-    //               {
-    //                 label: "Students",
-    //                 data: [
-    //                   individual_course.A,
-    //                   individual_course.B,
-    //                   individual_course.C,
-    //                   individual_course.D,
-    //                   individual_course.F,
-    //                   individual_course.P,
-    //                   individual_course.NP,
-    //                   individual_course.IX,
-    //                   individual_course.RD,
-    //                   individual_course.EW,
-    //                   individual_course.W,
-    //                 ],
-    //                 backgroundColor: "lightblue",
-    //               },
-    //             ],
-    //           }}
-    //         />
-    //       </div>
-    //     );
-    //   })}
-    // </div>
+    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,41 @@
 import { SetStateAction, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Router, { useRouter } from "next/router";
+import SchoolHeader from "../component/SchoolHeader";
 
 export default function Home() {
+  const router = useRouter();
   const [course, setCourse] = useState("");
 
-  function handleChange(event: any) {
+  function handleText(event: any) {
     let unformattedCourse = event.target.value;
     setCourse(unformattedCourse.toUpperCase());
   }
 
   return (
     <>
-      <h1>How Hard Is This Class?</h1>
-      <h4>
-        Welcome to the website that helps you decide which class at Santa Monica
-        College to take based on grade distributions.
-      </h4>
-      <div>
-        <input placeholder="Example: CS 8" onChange={handleChange}/>
-        <Link href={`/course/${course}`}>
-          <button>Go to Course</button>
-        </Link>
+      <SchoolHeader />
+      <div className="bg-slate-400 flex h-screen">
+
       </div>
+      {/* <div className="flex flex-col text-white h-screen justify-center items-center">
+        <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold py-8">
+          How Hard Is This Class?
+        </h1>
+        <div className="flex">
+          <input
+            className="text-black text-xl font-bold w-full md:w-1/2 h-10 rounded-full p-4"
+            placeholder="📖 Example: ACCTG 1"
+            onChange={handleText}
+          />
+          <Link className="ml-4" href={`/course/${course}`}>
+            <button className="border border-white rounded-full px-4 h-10 text-xl font-semibold hover:bg-gray-500">
+              Find Course
+            </button>
+          </Link>
+        </div>
+      </div> */}
     </>
   );
 }
